258d57c80aca818ff0880d1e19561a57de383b29
Andrii Symonenko <andrii.symonenko.dev@gmail.com>
2020-09-10 23:15:56 +0300

    add style for test popup and class for generate answer and question

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -3,57 +3,49 @@ const testCreate = (stateTest) => {
 
     const answerLabel = document.querySelectorAll('.test-popup-window--list > li > label');
 
-    class TestForm {
-        constructor(question, answer_1, answer_2, answer_3, parentSelector) {
+
+    class AnswerQuestionConstructor {
+        constructor(question, answer_1, answer_2, answer_3, parentQuestionSelector, parentAnswerSelector_1, parentAnswerSelector_2, parentAnswerSelector_3, ) {
             this.question = question;
             this.answer_1 = answer_1;
             this.answer_2 = answer_2;
             this.answer_3 = answer_3;
-            this.parent = document.querySelector(parentSelector);
+            this.parentQuestion = document.querySelector(parentQuestionSelector);
+            this.parentAnswer_1 = document.querySelector(parentAnswerSelector_1);
+            this.parentAnswer_2 = document.querySelector(parentAnswerSelector_2);
+            this.parentAnswer_3 = document.querySelector(parentAnswerSelector_3);
         }
 
-        render() {
-            const element = document.createElement('div');
-            element.classList.add('test-popup-container')
-            element.innerHTML = `
-            <h3>${(this.question)}</h3>
-    <ul class="test-popup-window--list">
-        <li class="test-popup-window--list__item">
-          <input type="checkbox" name="answer_1">
-          <label data-answer="1" for="answer_1">${(this.answer_1)}</label>
-        </li>
-        <li class="test-popup-window--list__item">
-          <input type="checkbox" name="answer_2">
-          <label data-answer="2" for="answer_2">${(this.answer_2)}</label>
-        </li>
-        <li class="test-popup-window--list__item">
-          <input type="checkbox" name="answer_3">
-          <label data-answer="3" for="answer_3">${(this.answer_3)}</label>
-        </li>
-    </ul>
-      
-    `;
-            this.parent.prepend(element);
+        renderQuize() {
+            this.parentQuestion.insertAdjacentHTML('afterbegin', `<h3>${this.question}</h3>`);
+            this.parentAnswer_1.insertAdjacentHTML('afterend', `<label data-answer="1" for="answer_1">${this.answer_1}</label>`);
+            this.parentAnswer_2.insertAdjacentHTML('afterend', `<label data-answer="2" for="answer_2">${this.answer_2}</label>`);
+            this.parentAnswer_3.insertAdjacentHTML('afterend', `<label data-answer="3" for="answer_3">${this.answer_3}</label>`);
         }
-    };
 
-    new TestForm(
+
+    }
+
+    new AnswerQuestionConstructor(
         'Lorem ipsum',
         'Lorem 1',
         'Lorem 2',
         'Lorem 3',
-        ".test-popup-window"
+        '.test-popup-container',
+        '#answer_1',
+        '#answer_2',
+        '#answer_3',
+    ).renderQuize();
 
-    ).render();
 
-    let testFormGet = document.querySelector('.test-popup-window > div > ul');
-    console.log(testFormGet);
-    testFormGet.addEventListener('input', (event) => {
-        let target = event.target
-        if (target.tagName === 'INPUT') {
-            console.log('input!')
-        }
-    });
+    // let testFormGet = document.querySelector('.test-popup-window > div > ul');
+    // console.log(testFormGet);
+    // testFormGet.addEventListener('input', (event) => {
+    //     let target = event.target
+    //     if (target.tagName === 'INPUT') {
+
+    //     }
+    // });
 
     // answerCheckbox.forEach(item => {
     //     item.addEventListener('input', () => {
@@ -63,7 +55,48 @@ const testCreate = (stateTest) => {
     //     })
     // });
 
+    // const questionObj = {
+    //     question_1: 'Lorem ipsum 1',
+    //     question_2: 'Lorem ipsum 2',
+    // }
 
+    // function getKey(obj) {
+    //     let arr = [];
+    //     for (let key in obj) {
+    //         arr.push(obj[key]);
+    //     }
+    //     return arr
+    // }
+
+    // let quest = getKey(questionObj);
+
+
+    // console.log(quest);
+
+    // function changeElementText(element, text) {
+    //     element.innerHTML = text;
+    // }
+
+
+    // let testFormGet = document.querySelector('.test-popup-window--list');
+    // let getInput = testFormGet.querySelectorAll('input');
+    // let getQuestion = document.querySelector('.test-popup-container > h3');
+
+    // function clearInput() {
+    //     getInput.forEach(item => {
+    //         item.checked = false;
+    //     });
+    // }
+
+    // //Rewrite to function
+    // testFormGet.addEventListener('click', (event) => {
+    //     let target = event.target;
+    //     if (target.tagName === 'INPUT') {
+
+    //         setTimeout(changeElementText, 700, getQuestion, questionObj.question_1);
+    //         setTimeout(clearInput, 700);
+    //     }
+    // })
 
 };
 
